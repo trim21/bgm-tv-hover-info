ae5f3f0f0f06187caf3f49344d56b2b0aa73cbe0
Trim21 <trim21.me@gmail.com>
2021-12-29 02:28:51 +0800

    add description

--- config/metadata.js
+++ config/metadata.js
@@ -4,15 +4,13 @@ module.exports = {
   name: name,
   'name:zh': '鼠标指向条目链接时显示更多信息',
   namespace: 'https://trim21.me/',
+  description: '在讨论贴中添加一个悬浮窗显示条目信息',
   version: version,
   author: author,
   source: repository.url,
   supportURL: repository.url + '/issues',
   license: 'MIT',
   match: ['https://bgm.tv/group/topic/*', 'https://bangumi.tv/group/topic/*', 'https://chii.in/group/topic/*'],
-  require: [
-    `https://cdn.jsdelivr.net/npm/jquery@${dependencies.jquery}/dist/jquery.min.js`,
-    `https://cdn.jsdelivr.net/npm/lodash@${dependencies.lodash}/lodash.min.js`,
-  ],
+  require: [`https://cdn.jsdelivr.net/npm/jquery@${dependencies.jquery}/dist/jquery.min.js`],
   'run-at': 'document-end',
 };
