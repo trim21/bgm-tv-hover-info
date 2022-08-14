20cae22354341456099453d7e341f5403f8c69ad
Trim21 <trim21.me@gmail.com>
2022-08-15 05:27:15 +0800

    chore: update deps

--- config/metadata.js
+++ config/metadata.js
@@ -1,12 +1,12 @@
 const { author, dependencies, name, repository, version } = require('../package.json');
 
 module.exports = {
-  name: name,
+  name,
   'name:zh': '鼠标指向条目链接时显示更多信息',
   namespace: 'https://trim21.me/',
   description: '在讨论贴中添加一个悬浮窗显示条目信息',
-  version: version,
-  author: author,
+  version,
+  author,
   source: repository.url,
   supportURL: repository.url + '/issues',
   license: 'MIT',
