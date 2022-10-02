1b7aee041932e8fbb23187062a2ed4da40bc87ac
trim21 <trim21@users.noreply.github.com>
2022-10-02 00:42:14 +0000

    deploy refs/tags/v0.2.1 330a801f9dea44dd6e6c6f8863e921977ddb4ec2

--- bgm-tv-hover-more-info.prod.user.js
+++ bgm-tv-hover-more-info.prod.user.js
@@ -3,7 +3,7 @@
 // @name:zh       鼠标指向条目链接时显示更多信息
 // @namespace     https://trim21.me/
 // @description   在讨论贴中添加一个悬浮窗显示条目信息
-// @version       0.2.0
+// @version       0.2.1
 // @author        Trim21 <i@trim21.me>
 // @source        https://github.com/Trim21/bgm-tv-hover-info
 // @supportURL    https://github.com/Trim21/bgm-tv-hover-info/issues
@@ -28,7 +28,28 @@ var __webpack_exports__ = {};
 
 ;// CONCATENATED MODULE: external "$"
 const external_$_namespaceObject = $;
+;// CONCATENATED MODULE: ./src/utils.ts
+
+function getSubjectID(s) {
+  if (!(s == null ? void 0 : s.length))
+    return void 0;
+  if (s.startsWith("/")) {
+    s = "https://bgm.tv" + s;
+  }
+  const u = new URL(s);
+  const path = u.pathname;
+  const split = path.split("/");
+  if (split.length >= 4) {
+    return void 0;
+  }
+  if (split[1] === "subject") {
+    return parseInt(split[2], 10);
+  }
+  return void 0;
+}
+
 ;// CONCATENATED MODULE: ./src/index.ts
+
 
 
 const style = `
@@ -75,27 +96,27 @@ function createPopup(subject) {
   var _a;
   let rank = "";
   if (subject.rating.rank) {
-    rank = `<p class="rateInfo">
-<span class="starstop-s"><span class="starlight stars${Math.round(subject.rating.score)}"></span></span>
- <small class="fade">${subject.rating.score}</small> <span class="tip_j">(${subject.rating.total}人评分)</span>
+    rank = `<p class='rateInfo'>
+<span class='starstop-s'><span class='starlight stars${Math.round(subject.rating.score)}'></span></span>
+ <small class='fade'>${subject.rating.score}</small> <span class='tip_j'>(${subject.rating.total}人评分)</span>
 </p>`;
   }
   let tags = "";
   if (subject.tags.length) {
     tags = "<div class='popup-tags'>" + subject.tags.sort((a, b) => b.count - a.count).slice(0, 10).map(
-      (value) => `<span class="tag"><span class="name">${value.name}</span> <small>${value.count}</small></span>`
+      (value) => `<span class='tag'><span class='name'>${value.name}</span> <small>${value.count}</small></span>`
     ).join("\n");
     tags += "</div>";
   }
   return `
-<div class="d-flex">
-  <span class="image d-block">
-    <img src="${(_a = subject.images) == null ? void 0 : _a.small}" class="cover" alt="${subject.name}">
+<div class='d-flex'>
+  <span class='image d-block'>
+    <img src='${(_a = subject.images) == null ? void 0 : _a.small}' class='cover' alt='${subject.name}'>
   </span>
-  <div class="d-block">
+  <div class='d-block'>
     <h3>${subject.name}</h3>
-    <small class="grey">${subject.name_cn}</small>
-    <p class="info tip"> ${subject.summary} </p>
+    <small class='grey'>${subject.name_cn}</small>
+    <p class='info tip'> ${subject.summary} </p>
   </div>
 </div>
 
@@ -108,15 +129,10 @@ async function main() {
   console.log(GM.info.script.name);
   external_$_namespaceObject("head").append(style);
   external_$_namespaceObject("a").each((i, e) => {
-    if (isBangumiSubjectHref(external_$_namespaceObject(e).attr("href"))) {
+    if (getSubjectID(external_$_namespaceObject(e).attr("href"))) {
       external_$_namespaceObject(e).on("mouseover", hoverHandler).on("mouseleave", leaveHandler);
     }
   });
-}
-function isBangumiSubjectHref(s) {
-  if (!(s == null ? void 0 : s.length))
-    return false;
-  return /\/subject\/\d+/.test(s) || /https:\/\/bgm.tv\/subject\/\d+/.test(s) || /https:\/\/bangumi.tv\/subject\/\d+/.test(s) || /https:\/\/chii.in\/subject\/\d+/.test(s);
 }
 async function leaveHandler() {
   external_$_namespaceObject("#popup").remove();
@@ -136,7 +152,7 @@ async function hoverHandler() {
     position: "absolute",
     "z-index": 1e3
   });
-  const subjectID = href.split("/").pop();
+  const subjectID = getSubjectID(href);
   if (!subjectID) {
     return;
   }
