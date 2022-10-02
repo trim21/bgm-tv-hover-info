778363081ce29779f967290c7835864d9ddf97fe
Trim21 <trim21.me@gmail.com>
2022-10-02 08:58:15 +0800

    build: add webpack cache

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -1,6 +1,10 @@
 const path = require('path');
 
 const webpackConfig = {
+  cache: {
+    type: 'filesystem',
+    allowCollectingMemory: true,
+  },
   resolve: {
     extensions: ['.js', '.ts'],
   },
